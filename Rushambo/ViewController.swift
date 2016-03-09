7b4735e917f496e6c10a42f482b8165bbc152e48
Youngsun Paik <youngsunpaik@gmail.com>
2016-03-09 09:58:57 -0800

    Initial Commit

---
+++ Rushambo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rushambo
+//
+//  Created by Youngsun Paik on 3/9/16.
+//  Copyright © 2016 Youngsun Paik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
